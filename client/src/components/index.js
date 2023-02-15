399a5abfb869e312feb9fddf8a1570f92cb2adf3
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-02-15 19:39:26 +0530

    server

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,6 @@ import Home from './Home';
 import Questions from './Questions';
 import Quiz from './Quiz';
 import Result from './Result';
-// import Questions from './Questions';
 
 export{
     Home,
