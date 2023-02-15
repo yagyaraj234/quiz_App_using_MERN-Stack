f9ae6d979af4f341421e06bf4c7221a933b8ba32
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-02-15 19:34:22 +0530

    second commit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,8 @@ import Home from './Home';
 import Questions from './Questions';
 import Quiz from './Quiz';
 import Result from './Result';
-Questions
+// import Questions from './Questions';
+
 export{
     Home,
     Quiz,
