46a7f4faf4716fa67664ee6e5d63a9f4180d5b8c
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-02-18 15:25:00 +0530

    m

--- client/src/redux/resultReducer.js
+++ client/src/redux/resultReducer.js
@@ -1,4 +1,4 @@
-// import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 export const resultReducer = createSlice({
     name:'result',
